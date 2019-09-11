41534c58e590edc305d7002f33f757230766ebd6
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-09-11 09:03:01 +0900

    화면 정가운데 frame 위치하기

--- 0906_GUI1/src/GUI1_JFrame.java
+++ 0906_GUI1/src/GUI1_JFrame.java
@@ -1,8 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI1_JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창 이름");
+        Dimension dimen = Toolkit.getDefaultToolkit().getScreenSize();
+        int 화w = dimen.width;
+        int 화h = dimen.height;
+        int 창w = 400;
+        int 창h = 200;
+
+        frame.setPreferredSize(new Dimension(창w,창h));
+        frame.setLocation((화w-창w)/2,(화h-창h)/2);
+
         JPanel panel = new JPanel();
         JLabel label = new JLabel("어쩌다 금요일 ㄱㅇㄷ");
         JButton button = new JButton("오늘 회식감");
