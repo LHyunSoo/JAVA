c5c706f6c837d71a19f0e7441fd8e58139380e42
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-09-11 10:08:19 +0900

    Components, label, button, checkbox, radiobutton, buttongroup, textfield, textarea, combobox

--- 0906_GUI1/src/Sample.java
+++ 0906_GUI1/src/Sample.java
@@ -3,9 +3,31 @@ import java.awt.*;
 
 public class Sample {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        String[] jinwoo = {"이진우","해남이","콩알이","라이언","또치","어린왕자"};
+        JFrame frame = new JFrame("Components");
         JPanel panel = new JPanel();
+        JLabel label = new JLabel("JLable");
+        JButton button = new JButton("JButton");
+        JCheckBox checkBox = new JCheckBox("JCheckBox");
+        JCheckBox checkBox1 = new JCheckBox("JCheckBox1");
+        JRadioButton radioButton = new JRadioButton("미성년자");
+        JRadioButton radioButton1 = new JRadioButton("성인");
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(radioButton);
+        buttonGroup.add(radioButton1);
+        JTextField textField = new JTextField("JTextField");
+        JTextArea textArea = new JTextArea(3,30);
+        JComboBox comboBox = new JComboBox(jinwoo);
 
+        panel.add(label);
+        panel.add(button);
+        panel.add(checkBox);
+        panel.add(checkBox1);
+        panel.add(radioButton);
+        panel.add(radioButton1);
+        panel.add(textField);
+        panel.add(textArea);
+        panel.add(comboBox);
         frame.add(panel);
 
         frame.setPreferredSize(new Dimension(600,400));
