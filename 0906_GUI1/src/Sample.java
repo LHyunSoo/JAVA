4707780262f9d79feaacaf2487ef15bdaecab62c
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-09-18 10:06:06 +0900

    button add ActionLister

--- 0906_GUI1/src/Sample.java
+++ 0906_GUI1/src/Sample.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Sample {
     public static void main(String[] args) {
@@ -18,6 +20,13 @@ public class Sample {
         JTextField textField = new JTextField("JTextField");
         JTextArea textArea = new JTextArea(3,30);
         JComboBox comboBox = new JComboBox(jinwoo);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button.setText("Clicked");
+            }
+        });
 
         panel.add(label);
         panel.add(button);
