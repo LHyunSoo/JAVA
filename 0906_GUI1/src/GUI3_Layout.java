c78b4905adb15355e38e70cdbd064977804e63c6
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-10-02 08:41:09 +0900

    FlowLayout, alignment

---
+++ 0906_GUI1/src/GUI3_Layout.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI3_Layout {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Components");
+        JPanel panel = new JPanel();
+
+        FlowLayout fl = new FlowLayout();   //JPanel의 기분 layout
+        fl.setAlignment(FlowLayout.LEFT);
+        panel.setLayout(fl);
+
+        for(int i = 0;i<6;i++){
+            JButton bt = new JButton("Button"+i);
+            panel.add(bt);
+        }
+
+        frame.add(panel);
+
+        frame.setPreferredSize(new Dimension(600,400));
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
