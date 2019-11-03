708075fa4f0872b1bf296615e2dedaf992a78b94
LHyunSoo <s2018w36@e-mirim.hs.kr>
2019-11-03 23:13:03 +0900

    머리스타일 추천 프로그램

--- 2학기/GUII/src/GUI8_ImageIcon.java
+++ 2학기/GUII/src/GUI8_ImageIcon.java
@@ -7,9 +7,11 @@ public class GUI8_ImageIcon {
         JPanel panel = new JPanel();
         ImageIcon iijpg = new ImageIcon(GUI8_ImageIcon.class.getResource("img/Instargram.jpg"));
         ImageIcon iijpg_edit = new ImageIcon(iijpg.getImage().getScaledInstance(iijpg.getIconWidth()/10,iijpg.getIconHeight()/10,Image.SCALE_DEFAULT));
-        JLabel lbii = new JLabel(iijpg_edit);
+        JLabel lbii = new JLabel(iijpg);
+        JLabel lbiie = new JLabel(iijpg_edit);
 
         panel.add(lbii);
+        panel.add(lbiie);
         frame.add(panel);
 
         frame.setPreferredSize(new Dimension(600,400));
